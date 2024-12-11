8a2b8dbf64cfbcc90ae8e993afa00690d15d7d34
MachineLearningHacks <80762630+MachineHacks@users.noreply.github.com>
2024-12-12 03:20:33 +0530

    Add files via upload

---
+++ main.R
@@ -0,0 +1,9 @@
+# main.R
+source("/app/plumber_app.R")
+source("/app/Test.R")
+source("/app/Tetsting.R")
+
+# If you want to start a Plumber API
+library(plumber)
+pr <- plumb("/app/plumber_app.R")
+pr$run(port = 8000, host = "0.0.0.0")
