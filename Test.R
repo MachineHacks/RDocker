27fffee304ae01d413830810769799a96843402b
MachineLearningHacks <80762630+MachineHacks@users.noreply.github.com>
2024-12-11 18:56:45 +0530

    Update Test.R

--- Test.R
+++ Test.R
@@ -1,8 +1,1 @@
-library(plumber)
-
-# Simple GET endpoint to check if the API is working
-#* @get /test
-function() {
-  print("The Docker container and Plumber API are working!")
-  return(list(message = "The Docker container and Plumber API are working!"))
-}
+print("Hello World of Docker")
