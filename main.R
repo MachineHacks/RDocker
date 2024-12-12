9e1eebd868648c806f9c2d76848d37cc357e1ae6
MachineLearningHacks <80762630+MachineHacks@users.noreply.github.com>
2024-12-12 19:52:18 +0530

    Update main.R

--- main.R
+++ main.R
@@ -10,4 +10,4 @@ source("/app/UpdatedPlumber.R")
 # If you want to start a Plumber API
 library(plumber)
 pr <- plumb("/app/UpdatedPlumber.R")
-pr$run(port = 8000, host = "0.0.0.0")
+pr$run(port = 5000, host = "0.0.0.0")
