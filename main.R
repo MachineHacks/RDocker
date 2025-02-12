25e665f1c6b9b628b4490a18712d343ef742de47
MachineLearningHacks <80762630+MachineHacks@users.noreply.github.com>
2025-02-12 21:24:11 +0530

    Update main.R

--- main.R
+++ main.R
@@ -1,9 +1,30 @@
 # main.R
-source("/app/UpdatedPlumber.R")
-#source("/app/Test.R")
-#source("/app/Tetsting.R")
 
-# If you want to start a Plumber API
 library(plumber)
-pr <- plumb("/app/UpdatedPlumber.R")
+library(jsonlite)
+
+# Load utility functions
+source("/app/Utility.R")  
+config <- load_config("/app/Config.json")  
+
+# Extract API metadata dynamically
+api_title <- config$API$TITLE
+api_description <- config$API$DESCRIPTION
+
+# Initialize Plumber API
+pr <- plumber::plumb("/app/UpdatedPlumber.R")
+
+# Set API Metadata (Title & Description)
+pr$setDocs(
+  list(
+    openapi = "3.0.3",
+    info = list(
+      title = api_title,
+      description = api_description,
+      version = "1.0.0"
+    )
+  )
+)
+
+# Run Plumber API
 pr$run(port = 8000, host = "0.0.0.0")
